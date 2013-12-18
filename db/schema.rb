f59c760a7993088d1055a8b71edb3483257619ef
Lorry Rocha <sorai120@yahoo.com>
2013-12-17 22:15:44 -0500

    Added Rspec and Factory girl. Need to write tests to move on to step 7

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218025248) do
+ActiveRecord::Schema.define(version: 20131218025541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20131218025248) do
     t.datetime "updated_at"
     t.string   "author"
     t.integer  "rating"
+  end
+
+  create_table "checkouts", force: true do |t|
+    t.integer  "book_id",    null: false
+    t.string   "reader"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
